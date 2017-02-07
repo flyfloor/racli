d617c8e20eb839408589a2c2263a56d1c11309d8
jerryshew <jerryshew@gmail.com>
2017-02-07 17:01:08 +0800

    scratch

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    repo_url: 'https://github.com/jerryshew/react-boilerplate/archive/master.zip',
+    questions: [
+        {
+            type: 'input',
+            name: 'version',
+            message: 'project version',
+            validate: function(input){
+                return /^([0-9]{1,3}\.){2}[0-9]{1,3}$/.test(input)
+            },
+            default: '0.0.1'
+        }, {
+            type: 'input',
+            name: 'author',
+            message: 'author'
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'description'
+        }, {
+            type: 'input',
+            name: 'homepage',
+            message: 'project home page(e.g. https://github.com)'
+        }, {
+            type: 'input',
+            name: 'keywords',
+            message: 'keywords(e.g. react git)',
+            default: 'react',
+        }
+    ],
+    username: 'jerryshew',
+    repo: 'react-boilerplate',
+    gitignore: '.DS_Store\nnode_modules\n/temp\n/tmp\n.log'
+}
---
+++ init.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node --harmony
+
+const inquirer = require('inquirer')
+const program = require('commander')
+const download = require('download-git-repo')
+const fs = require('fs')
+
+const answer = require('./lib/answer')
+const generate = require('./lib/generate')
+const install = require('./lib/install')
+
+let config = require('./config')
+let questions = config.questions
+
+program
+    .version(require('./package').version)
+    .arguments('<pName>')
+    .option('-u --username <username>', 'github user login')
+    .option('-r --repo <repo>', 'github user repository')
+    .action(pName => {
+        // check project if exist
+        if (/(\/|\\|\s)+/.test(pName)) {
+            return console.error("project name can't have '\', '/' or emty space ")
+        }
+        if (ifExistProject(pName)) {
+            console.error('project already exist')
+            // return 
+        }
+
+        // download boilerplate
+        // downloadTemplate({ username: program.username, repo: program.repo }, pName)
+        //     .then(res => {
+        //         // questions
+        //         answer(questions, answers => {
+        //             answers.name = pName
+        //             // generate package.json
+        //             generate(pName, answers)
+        //             // install
+        //         })
+        //     })
+
+        install(pName)
+        
+    })
+    .parse(process.argv)
+
+
+// private
+
+function ifExistProject(name) {
+    return fs.existsSync(`${process.cwd()}/${name}`)
+}
+
+function downloadTemplate({ username=config.username, repo=config.repo }, pathName) {
+    console.log('downloading from', username, '/', repo, '...')
+    return new Promise((resolve, reject) => {
+        download(`${username}/${repo}`, pathName, function(err){
+            if (err) return console.error(err)
+            console.log('downloading done.')
+            resolve(true)
+        })
+    });
+}
